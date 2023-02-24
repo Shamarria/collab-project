fc9e0f151f4f02a99c45d7531dfdea6390d8a8e7
Shamarria <126272225+Shamarria@users.noreply.github.com>
2023-02-24 10:29:43 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let mySprite = sprites.create(img`
+    . . . . . . . . b b b b . . . . 
+    . . . . b b b b 3 3 3 3 b . . . 
+    . c c b b 1 1 3 3 3 3 3 b b . . 
+    c c 3 3 1 1 3 3 3 3 3 1 1 b . . 
+    c b 3 3 3 3 3 3 3 3 3 1 1 b . . 
+    f b b c c c 3 3 3 3 3 3 3 c . . 
+    f b c c c b b b b 3 3 3 3 3 c . 
+    f b c c d d d d d b b 3 3 3 3 c 
+    . c c d d c d d c d d b c 3 3 c 
+    . c b d d c d d c d d c c c 3 f 
+    . f d d d d d d d d d c c c b f 
+    . f d c d d d d c d d c c c b f 
+    . . c d c c c c d d b c b b f f 
+    . . f f d d d d c c b f f f f . 
+    . f f b b f f c c b d d b f . . 
+    . f b b b f f . . b d d d f . . 
+    `, SpriteKind.Player)
